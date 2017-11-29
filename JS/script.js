a8794db3fe15c03956d91a51aac720450133bfb4
MarioClot <mariorrsv@gmail.com>
2017-11-29 19:22:16 +0100

    BarChart fet

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,7 @@ var columnsSize;
 var arrayValorRows;
 var arrayNomRows;
 var table;
+var sumaPerRows;
 
 function inici(){
     var myCanvas = document.getElementById("area");
@@ -88,23 +89,39 @@ function agafaNameRows() {
     //alert(arrayNomRows.toString());
 }
 
-var sumaPerRows=[];
+//var sumaPerRows=[];
+function sumarValorsRows(){
+    var table = document.getElementById("taula");
+    var totalRows = table.rows.length-2;
+    window.sumaPerRows=[];
+    for (var i=0;i<totalRows;i++){
+        var sumaRow=0;
+        for ( var num in window.arrayValors[i]){
+            sumaRow += window.arrayValors[i][num];                
+        }
+        window.sumaPerRows[i]=sumaRow;
+    }
+    alert(sumaPerRows.toString());
+}
+
 
 function crearGrafica(){
-    var totalRows = table.rows.length-2;
-    sumaPerRows=[];
-    function sumarValorsRows(x){
+    /*var totalRows = table.rows.length-2;
+    window.sumaPerRows=[];
+    function sumarValorsRows(){
         
         for (var i=0;i<totalRows;i++){
             var sumaRow=0;
             for ( var num in arrayValors[i]){
                 sumaRow += arrayValors[i][num];                
             }
-            sumaPerRows[i]=sumaRow;
+            window.sumaPerRows[i]=sumaRow;
         }
         alert(sumaPerRows.toString());
     }
-    sumarValorsRows(1);
+*/
+
+    sumarValorsRows();
 
     myCanvas.width = 300;
     myCanvas.height = 300;
@@ -248,11 +265,11 @@ function crearLineChart(){
 
 
 function barChar(){
+
+            sumarValorsRows();
     
             lineChart.width = 600;  //le asignamos el ancho al total del canvas
             lineChart.height = 400; //le asignamos la altura total del canvas
-            
-            //var dadesVert = table.rows[1].cells.length-2;  //guardamos el total de columnas que habra que señalar con una rallita en el eje de abajo (el de las X)
             var dadesVert = table.rows.length-2;
             var offset = 50;  //le asignamos a la variable offset 50 para que el dibujo no se quede oculto en ninguno bo de los bordes
             var alturaGrafic = lineChart.height -2 * offset;  //la altura de el eje de las Y (vertical) sera el total del alto del canvas menos dos veces el offset (350-2*50), para los margenes de arriba y abajo
@@ -261,6 +278,8 @@ function barChar(){
             var vStep = alturaGrafic / dadesVert; //la separacion horizontal entre los puntos en el eje de las X sera la anchura del eje X entre el total de datos
             var separacio = vStep / 5;
             var hStep = 50;  //la separacion entre puntos del eje Y sera de 50
+            var widthBarra = alturaGrafic / dadesVert - separacio*2;
+
             ctx2.beginPath();  //iniciamos un camino o dibujo
             ctx2.moveTo(offset,offset);   //nos movemos al punto (50,50)
             ctx2.lineTo(offset,alturaGrafic+offset); //dibujamos una linia hacia el punto (50,300)
@@ -268,22 +287,21 @@ function barChar(){
             ctx2.stroke();  //pintamos el camino
             dibujaHorizontal();     //dibujamos las cordenadas horizontales
             dibujaVertical();     //dibujamos las cordenadas verticales
+
+            console.log(dadesVert);
+            console.log(window.sumaPerRows.toString());
             var colors = ["#ff0000","#00008b","#00fa9a","#ffd700","#5fcdee","#c465e5"];  //array donde guardamos los colores de las linias
-            /*for ( var x = 0; x < table.rows.length; x++){  //desde 0 hasta que alcancemos el numero de filas de la tabla
+            for ( var x = 0; x < dadesVert; x++){  //desde 0 hasta que alcancemos el numero de filas de la tabla
                 dibujaGrafico(x,colors[x]);     //dibujamos el grafico con el color que le toca
-            }*/
+            }
     
             function dibujaHorizontal(){
                 ctx2.beginPath();       //empezamos un camino o dibujo
                 
                 ctx2.moveTo(offset,alturaGrafic+offset);        //nos movemos al punto (50,300)
-              //  for ( var i = 0; i <= dadesHor; i++){       //desde 0 hasta que alcancemos el total de columnas que habra en la tabla
                 for ( var i = 0; i <=10; i++){
-                    console.log("soy retrasao");        //reafirmo mi inutilidad para comprobar que todo funcionaba
-    
                     ctx2.moveTo(offset+i*hStep,alturaGrafic+offset);        // nos movemos al punto (50 + i*separacion horizontal entre datos , 300)
                     ctx2.lineTo(offset+i*hStep,offset+alturaGrafic+5);      // pintamos una linia desde el punto anterior hacia 5 pixeles mas abajo, para marcar el eje X de cordenadas
-                    //ctx2.fillText(window.arrayNomColumns[i], offset +i*hStep-10+hStep,  offset + alturaGrafic + 20);       //escribimos a cada rallita el nombre de la columna que le toque
                     ctx2.fillText((i*hStep)/10,offset-5 +i*hStep,offset+alturaGrafic+15);
                 }
                 ctx2.stroke();      // repasamos la linia
@@ -294,10 +312,7 @@ function barChar(){
                 
                 ctx2.moveTo(offset,alturaGrafic+offset);         //nos movemos al punto (50,300)
                 for ( var i = 0; i < dadesVert; i++){       //desde 0 hasta 4 que sera la cantidad de marcas que habra en el eje vertical
-                    
-                    console.log("soy retrasao");        //reafirmo mi inutilidad otra vez para comprobar que todo funcionaba
                     ctx2.moveTo(offset,alturaGrafic+offset - i*vStep +separacio*i);        // nos movemos al punto (50, 250+50 -  + i*separacion vertical entre marcas )
-                    //ctx2.lineTo(offset-5,alturaGrafic+offset - i*vStep+separacio*i);      // pintamos una linia desde el punto anterior hacia 5 pixeles a la izquierda, para marcar el eje Y de cordenadas
                     ctx2.fillText(window.arrayNomRows[i],offset -45,offset+alturaGrafic- i*vStep +separacio*i-vStep+separacio+2);
                 }
                 ctx2.stroke();      // repasamos la linia
@@ -305,20 +320,11 @@ function barChar(){
     
             function dibujaGrafico(x,color){
                 ctx2.beginPath();       //empezamos el camino o dibujo
-                ctx2.strokeStyle = color;          //le asignamos un color a la linia
                 ctx2.fillStyle = color;
-                ctx2.lineWidth = 2;     // ancho de la linia dibujada sera de 2
-                //ctx2.moveTo(offset,alturaGrafic+offset);        // nos movemos al punto (50, 300)
-                ctx2.moveTo(offset+hStep,alturaGrafic+offset-arrayValors[x][0]*10);
-                for( var i = 1; i <= dadesHor; i++){       //desde i = 1 hasta la ultima row (de la fila en concreto)
-                    var ultimPuntX = offset + hStep*i;      //variable temporal en la que guardamos la cordenada del punto X actual ( sera la separacion entre puntos horizontal )
-                    var ultimPuntY = alturaGrafic+offset-arrayValors[x][i-1]*10;        //variable temporal en la que guardamos la cordenada del punto Y actual ( sera la altura del grafico + 50 - el valor que queramos representar*10, para escalarlo)
-                    ctx2.lineTo(ultimPuntX,ultimPuntY);     // dibujamos la linia hasta el punto en cuestion
-                    ctx2.fillRect(ultimPuntX-1.5,ultimPuntY-1.5,3,3)
-                    ctx2.moveTo(ultimPuntX,ultimPuntY);     //nos movemos hasta el punto en cuestion para en la siguiente iteracion dibujar ya desde ahi
-                    
-                }
-                ctx2.stroke();      //repasamos la linia
+                ctx2.moveTo(offset,alturaGrafic+ x*vStep +separacio*x-vStep+separacio+2);
+                var ultimPuntX = offset;      //variable temporal en la que guardamos la cordenada del punto X actual ( sera la separacion entre puntos horizontal )
+                var ultimPuntY =  offset+alturaGrafic- x*vStep +separacio*x-vStep+separacio-widthBarra/2;
+                ctx2.fillRect(ultimPuntX,ultimPuntY,sumaPerRows[x]*10,widthBarra);     // dibujamos la linia hasta el punto en cuestion
             }
         }
     }
